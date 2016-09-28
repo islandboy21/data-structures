80c1937af2f884b62a6b0c2e6f8351f721f49996
Henrique <hdeschain02@gmail.com>
2016-09-28 00:39:22 -0300

    Update linked-list.c
    Inserting a node at beginning.

--- linked-list.c
+++ linked-list.c
@@ -4,6 +4,40 @@
 typedef struct Node {
 	
 	int data;
-	Node* next;
+	struct Node* next;
 
-} Node;
+};
+
+	struct Node* head;
+
+void insert(int x) {
+
+	Node* temp = (Node*) malloc(sizeof(Struct Node));
+	temp->data = x;
+	temp->next = NULL;
+	
+	if(head != NULL){
+		temp->next = head;
+		head = temp;
+		
+	head = temp; // Why head receive temp?
+}
+	
+void Print() {
+	
+	struct Node* temp = head;
+	
+		while(temp != NULL) {
+			printf("%d", temp->data);
+			temp = temp->next;
+		}
+		printf("/n");
+}
+
+int main() {
+	
+	head = NULL;
+	
+	
+	return 0;
+}
